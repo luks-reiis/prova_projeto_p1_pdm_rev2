6352160a60fd605d2eaf1e2603bcaa637525ff9f
Lucas Reis <luk.wik40@gmail.com>
2025-09-25 23:02:59 -0300

    feat(p1): cria componente principal e faz hello

---
+++ projetoP1/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className='container mt-5'>
+      <div className='row'>
+        <div className='col-sm-12 text center'>Hello, Lembretes</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
